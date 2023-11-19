2de9fcc8a66f40fc602e143cba3bef288f0d0775
Li-Stark <liyd19@lzu.edu.cn>
2023-11-19 00:22:29 -0700

    config file

--- config.py
+++ config.py
@@ -8,20 +8,16 @@ Created on Sun Nov 21 16:59:37 2021
 class Config(object):
     def data_choose():
 
-        # train_file = 'H:/Yande_Li/ExpressDataset/Facemer/train(FER+)_aligned.txt'
-        # test_file = 'H:/Yande_Li/ExpressDataset/Facemer/test(FER+)_aligned.txt'
         train_file = 'H:/Yande_Li/ExpressDataset/Facemer/train(RAFDB).txt'
         test_file = 'H:/Yande_Li/ExpressDataset/Facemer/test(RAFDB).txt'
-        # train_file = 'H:/Yande_Li/ExpressDataset/Facemer/train(SFEW).txt'
-        # test_file = 'H:/Yande_Li/ExpressDataset/Facemer/test(SFEW).txt'
         return train_file, test_file
 
     exp_lr = 0.0001
     batch_size = 16
     epochs = 30
     momentum = 0.9
-    step_size = 30  # adjust learning rate after 25 epoch
-    gamma = 0.1     # lr =  lr/10
+    step_size = 30  
+    gamma = 0.1     
     outputdir = 'H:/Yande_Li/Facemer/saved_model'
     exp_dataset = 'RAFDB'
     device = 'cuda:0'
